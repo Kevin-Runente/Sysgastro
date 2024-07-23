978e5cbf4ca91c92e3c27ea67f687e4489a76ee9
Kevin González <21307019@utcgg.edu.mx>
2024-07-22 19:38:15 -0600

    includes

---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+<?php include 'header.php'; ?>
+
+<script src="../assets/dist/js/bootstrap.bundle.min.js"></script>
+
+<div class="container">
+  <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+    <p class="col-md-4 mb-0 text-body-secondary">&copy; Servicios de Tecnologías de Información - 2024</p>
+  </footer>
+</div>
